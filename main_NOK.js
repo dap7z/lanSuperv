dbf369339831f8a893b89c25aabf6eb1241e0188
Unknown <damien.pointier@gmail.com>
2018-05-10 17:10:01 +0200

    try eletron updater through github

--- main_NOK.js
+++ main_NOK.js
@@ -5,22 +5,17 @@ const {app, BrowserWindow, Menu, protocol, ipcMain} = require('electron');
 const log = require('electron-log');
 const {autoUpdater} = require("electron-updater");
 
+
 //-------------------------------------------------------------------
 // Logging
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This logging setup is not required for auto-updates to work,
-// but it sure makes debugging easier :)
 //-------------------------------------------------------------------
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
+
 //-------------------------------------------------------------------
-// Define the menu
-//
-// THIS SECTION IS NOT REQUIRED
+// Menu
 //-------------------------------------------------------------------
 let template = []
 if (process.platform === 'darwin') {
@@ -43,14 +38,9 @@ if (process.platform === 'darwin') {
 }
 
 
+
 //-------------------------------------------------------------------
-// Open a window that displays the version
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This isn't required for auto-updates to work, but it's easier
-// for the app to show a window than to have to click "About" to see
-// that updates are working.
+// Window that displays the version and working update
 //-------------------------------------------------------------------
 let win;
 
@@ -64,9 +54,10 @@ function createDefaultWindow() {
   win.on('closed', () => {
     win = null;
   });
-  win.loadURL(`file://${__dirname}/version.html#v${app.getVersion()}`);
+  win.loadURL(`file://${__dirname}/main.html#v${app.getVersion()}`);
   return win;
 }
+
 autoUpdater.on('checking-for-update', () => {
   sendStatusToWindow('Checking for update...');
 })
@@ -77,8 +68,9 @@ autoUpdater.on('update-not-available', (info) => {
   sendStatusToWindow('Update not available.');
 })
 autoUpdater.on('error', (err) => {
-  sendStatusToWindow('Error in auto-updater. ' + err);
+  sendStatusToWindow('Error in auto-updater.');
 })
+
 autoUpdater.on('download-progress', (progressObj) => {
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
@@ -86,8 +78,9 @@ autoUpdater.on('download-progress', (progressObj) => {
   sendStatusToWindow(log_message);
 })
 autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
+  sendStatusToWindow('Update downloaded; will install in 5 seconds');
 });
+
 app.on('ready', function() {
   // Create the Menu
   const menu = Menu.buildFromTemplate(template);
@@ -95,48 +88,28 @@ app.on('ready', function() {
 
   createDefaultWindow();
 });
-app.on('window-all-closed', () => {
-  app.quit();
-});
 
-//
-// CHOOSE one of the following options for Auto updates
-//
 
 //-------------------------------------------------------------------
-// Auto updates - Option 1 - Simplest version
+// Auto updates
 //
-// This will immediately download an update, then install when the
-// app quits.
-//-------------------------------------------------------------------
-app.on('ready', function()  {
-  autoUpdater.checkForUpdatesAndNotify();
-});
-
-//-------------------------------------------------------------------
-// Auto updates - Option 2 - More control
-//
-// For details about these events, see the Wiki:
 // https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
-//
 // The app doesn't need to listen to any events except `update-downloaded`
-//
-// Uncomment any of the below events to listen for them.  Also,
-// look in the previous section to see them being used.
 //-------------------------------------------------------------------
-// app.on('ready', function()  {
-//   autoUpdater.checkForUpdates();
-// });
-// autoUpdater.on('checking-for-update', () => {
-// })
-// autoUpdater.on('update-available', (info) => {
-// })
-// autoUpdater.on('update-not-available', (info) => {
-// })
-// autoUpdater.on('error', (err) => {
-// })
-// autoUpdater.on('download-progress', (progressObj) => {
-// })
-// autoUpdater.on('update-downloaded', (info) => {
-//   autoUpdater.quitAndInstall();  
-// })
+autoUpdater.on('update-downloaded', (info) => {
+  // Wait 5 seconds, then quit and install
+  setTimeout(function() {
+    autoUpdater.quitAndInstall();  
+  }, 5000)
+})
+
+
+//-------------------------------------------------------------------
+// Application
+//-------------------------------------------------------------------
+app.on('ready', function()  { 
+    autoUpdater.checkForUpdates();
+    
+    //include server.js
+	console.log("include server.js (OR FORK PROCESS)");
+});

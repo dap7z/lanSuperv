c9b70698ea9ec0476821268a778e5a6884b7a6cb
Unknown <damien.pointier@gmail.com>
2018-10-09 23:18:51 +0200

    function.js as class

--- functions.js
+++ functions.js
@@ -8,28 +8,34 @@ const Path = require('path');
 const {fork} = require('child_process');
 
 
-//public functions
-module.exports = {
-    
-    getDirectories: function(p){
-        var dirs =  Fs.readdirSync(p).filter(function (file) {
+class F {
+
+
+    constructor() {
+        console.log("class F constructor just called :)");
+    }
+
+
+    static getDirectories(p){
+        let dirs =  Fs.readdirSync(p).filter(function (file) {
             return Fs.statSync(p+'/'+file).isDirectory();
         });
-        dirsPaths = [];
+        let dirsPaths = [];
         dirs.map(function (dir) {
             dirsPaths.push(Path.join(p, dir));
         });
         return dirsPaths;
-    },
-    
-    
-    getPlugins: function(type='all',result='dirPath', format='object'){
-        var results;
-        var pluginsDirPath;
+    }
+
+
+    static getPlugins(type='all',result='dirPath', format='object'){
+        let results;
+        let pluginsDirPath;
+        let pluginsDirName;
         switch(type){
             case 'all':
-                var remoteRequestsPlugins = this.getDirectories(__dirname+'/plugins/remote-requests/');
-                var localResponsesPlugins = this.getDirectories(__dirname+'/plugins/local-responses/');
+                let remoteRequestsPlugins = this.getDirectories(__dirname+'/plugins/remote-requests/');
+                let localResponsesPlugins = this.getDirectories(__dirname+'/plugins/local-responses/');
                 pluginsDirPath = remoteRequestsPlugins.concat(localResponsesPlugins);
                 break;
             case 'remote':
@@ -55,8 +61,8 @@ module.exports = {
         //format results :
         if(format==='object'){
             //array to object for gun.js compatibility
-            var obj = {};
-            var pluginsId = 0;
+            let obj = {};
+            let pluginsId = 0;
             results.forEach(function(key) {
                 pluginsId += 1;
                 obj['plugin'+pluginsId] = key;
@@ -64,22 +70,22 @@ module.exports = {
             results = obj;
         }
         return results;
-    },
-    
-    
-    pcObject: function(params, THIS_PC, diagCallFrom=''){
-        var lanInterface = THIS_PC.lanInterface;
-        var wanInterface = THIS_PC.wanInterface;
-        var expectedParams = ['hostname', 'lastCheck', 'lanIP', 'lanMAC'];
-        var missingSomeParams = false;
+    }
+
+
+    static pcObject(params, THIS_PC, diagCallFrom=''){
+        let lanInterface = THIS_PC.lanInterface;
+        let wanInterface = THIS_PC.wanInterface;
+        let expectedParams = ['hostname', 'lastCheck', 'lanIP', 'lanMAC'];
+        let missingSomeParams = false;
         expectedParams.forEach(function(paramKey) {
             if(!params[paramKey]){
                 params[paramKey]='';
                 console.log('WARNING! pcObject() missing parameter: '+ paramKey);
                 missingSomeParams = true;
-            } 
+            }
         });
-        pc = params;
+        let pc = params;
         pc.online = true;
         pc.nickname = '';
         pc.lanNetwork = lanInterface.network;
@@ -95,21 +101,21 @@ module.exports = {
         //    console.log(pc);
         //}
         return pc;
-    },
+    }
 
 
-    checkData: function(THIS_PC, respondTo){
-        var params = {
+    static checkData(THIS_PC, respondTo){
+        let params = {
             hostname: THIS_PC.hostnameLocal,
             lastCheck: new Date().toISOString(),
             lanIP: THIS_PC.lanInterface.ip_address,
             lanMAC: THIS_PC.lanInterface.mac_address,
             machineID: THIS_PC.machineID
         };
-        var pc = this.pcObject(params, THIS_PC);
+        let pc = this.pcObject(params, THIS_PC);
         //each plugins as a key of pc object:
-        var plugins = this.getPlugins('all','dirName');
-        for (var key in plugins) {
+        let plugins = this.getPlugins('all','dirName');
+        for (let key in plugins) {
             pc[key] = plugins[key];
         }
         //respondsTo information:
@@ -117,27 +123,27 @@ module.exports = {
             pc['respondsTo-'+respondTo] = true;
         }
         return pc;
-    },
-    
-    
-    getPcIdentifier: function(pc){
-        var idPC = pc.lanMAC;   //computer identifier (simplified MAC adress)
+    }
+
+
+    static getPcIdentifier(pc){
+        let idPC = pc.lanMAC;   //computer identifier (simplified MAC adress)
         //used as unique identifier (it's supposed to be and we cant machine-id if app is not installed)
         if(idPC){
             idPC = idPC.replace(new RegExp(':', 'g'), '');
         }
         return idPC;
-    },
-    
-    
-    eventRedirection: function(eventData, dbComputers, method='http'){
-        var pcTarget = eventData.pcTarget;
-        var eventName = eventData.eventName;
-        
+    }
+
+
+    static eventRedirection(eventData, dbComputers, method='http'){
+        let pcTarget = eventData.pcTarget;
+        let eventName = eventData.eventName;
+
         console.log('[PLUGIN '+ eventName +']: local execution only => resend event through socket');
         //console.log('pcTarget');
         //console.log(pcTarget);
-        
+
 
 
         //Search computer that have the same machineID in (gun.js bdd|local array!) and get his actual IP:
@@ -146,7 +152,7 @@ module.exports = {
 
 
         //Retrieve pc info from database :
-        var idTargetPC = this.getPcIdentifier(pcTarget);
+        let idTargetPC = this.getPcIdentifier(pcTarget);
         dbComputers.get(idTargetPC).once(function(pcTarget, id){
             //necessite dbComputers en parametre fonction eventRedirection...
 
@@ -159,33 +165,33 @@ module.exports = {
             {
                 //HALF WORKING (form post data is not sended => selfTarget => OK FOR ONE REDIRECTION, NOT MORE)
                 //TODO: TESTS AND DEV
-                
+
                 //====[HTTP]====
-                var Request = require('request');
+                let Request = require('request');
                 // Set the headers
-                var headers = {
+                let headers = {
                     'User-Agent':       'LanSuperv Agent/1.0.0',
                     'Content-Type':     'application/x-www-form-urlencoded'
                 };
 
-                var jsonString = JSON.stringify({
+                let jsonString = JSON.stringify({
                     'eventName': eventName,
                     'pcTarget': pcTarget,
                     'password' : '*not*Implemented*',
                 });
 
-                var reqUrl = 'http://'+ pcTarget.lanIP +':'+ Config.val('SERVER_PORT') + Config.val('PATH_HTTP_EVENTS') +'/'+ eventName;
+                let reqUrl = 'http://'+ pcTarget.lanIP +':'+ Config.val('SERVER_PORT') + Config.val('PATH_HTTP_EVENTS') +'/'+ eventName;
                 console.log('[eventRedirection with http] reqUrl: '+reqUrl);
 
                 // Configure the request
-                var options = {
+                let options = {
                     url: reqUrl,
                     method: 'POST',
                     headers: headers,
                     form: {'jsonString': jsonString}
                 };
 
-                Request(options, function(err, res, body) {  
+                Request(options, function(err, res, body) {
                     if(err)
                     {
                         console.log('Error '+ err.code +' '+ reqUrl);
@@ -197,29 +203,29 @@ module.exports = {
                         console.log(body);
                     }
                 });
-                
+
                 //===============
             }
             else
             {
                 console.log('[error] function eventRedirection: unknow method parameter');
             }
-            
-        });
-        
-    },
-    
-    
-    eventExecution: async function(eventParams){
-        return new Promise(function (resolve) {
-            var eventName = eventParams.eventName;
-            var execPath = eventParams.execPath;
 
-            var lastObjectMsg = {};
-            var compute = fork(execPath);
+        });
+
+    }
+
+
+    static async eventExecution(eventParams){
+        return new Promise(function (resolve) {
+            let eventName = eventParams.eventName;
+            let execPath = eventParams.execPath;
+
+            let lastObjectMsg = {};
+            let compute = fork(execPath);
             compute.send(eventParams);
             compute.on('message', (msg) => {
-                var text = '[PLUGIN ' + eventName + '] message: ';
+                let text = '[PLUGIN ' + eventName + '] message: ';
                 if (typeof msg === 'object') {
                     //console.log(text);
                     //console.log(msg);
@@ -234,11 +240,11 @@ module.exports = {
                 }
             });
         });
-    },
+    }
 
 
     //http://2ality.com/2015/08/es6-map-json.html
-    strMapToObj: function(strMap) {
+    static strMapToObj(strMap) {
         let obj = Object.create(null);
         for (let [k,v] of strMap) {
             // We don’t escape the key '__proto__'
@@ -246,33 +252,32 @@ module.exports = {
             obj[k] = v;
         }
         return obj;
-    },
-    objToStrMap: function(obj) {
+    }
+    static objToStrMap(obj) {
         let strMap = new Map();
         for (let k of Object.keys(obj)) {
             strMap.set(k, obj[k]);
         }
         return strMap;
-    },
-    strMapToJson: function(strMap) {
+    }
+    static strMapToJson(strMap) {
         return JSON.stringify(this.strMapToObj(strMap));
-    },
-    jsonToStrMap: function(jsonStr) {
+    }
+    static jsonToStrMap(jsonStr) {
         return this.objToStrMap(JSON.parse(jsonStr));
-    },
+    }
 
 
-
-    logCheckResult: function(checkType, pcToUpdate) {
-        var log = "##Promise## AFTER " + checkType + "Check() UPDATE PC " + pcToUpdate.lanIP + " IN DATABASE  (";
+    static logCheckResult(checkType, pcToUpdate) {
+        let log = "##Promise## AFTER " + checkType + "Check() UPDATE PC " + pcToUpdate.lanIP + " IN DATABASE  (";
         if (!pcToUpdate['respondsTo-' + checkType]) {
             log += "NOT ";
         }
         log += "respondsTo-" + checkType + ")";
 
         console.log(log);
-    },
-    logCheckWarning: function(checkType, dbComputers, finalResult) {
+    }
+    static logCheckWarning(checkType, dbComputers, finalResult) {
         if (typeof dbComputers === 'undefined') {
             console.log("WARNING! [" + checkType + "] gun.js dbComputers required !");
         }
@@ -282,3 +287,6 @@ module.exports = {
     }
 
 };
+
+
+module.exports = F;

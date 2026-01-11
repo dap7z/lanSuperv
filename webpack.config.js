bec09d97c4f12fa43e5127caa9d76178cb44a126
damien <damien.pointier@gmail.com>
2026-01-11 18:56:39 +0100

    update npm packages

--- webpack.config.js
+++ webpack.config.js
@@ -24,8 +24,14 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.vue$/,
-                loader: 'vue-loader'
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
             }
         ]
     }
